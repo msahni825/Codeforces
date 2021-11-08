5e26450f6aaf238551b3513bd68de3452b247a2b
Megha Sahni <42293547+msahni825@users.noreply.github.com>
2021-11-08 16:49:03 +0530

    A. Hexadecimal's theorem

---
+++ Ladder A/Div2-125A.cpp
@@ -0,0 +1,13 @@
+/*A. Hexadecimal's theorem
+3	=>	1 1 1
+13	=>	2 3 8
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	cout<<0<<" "<<0<<" "<<n<<"\n";
+	return 0;
+}
